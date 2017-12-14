5a3d06dea65d46e620988ac2647fcba2433de407
joeldiaztoledo <joeldiaztoledo@gmail.com>
2017-12-14 20:49:33 +0000

    Commit inicial

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	
+		int i = 0;
+		for(i=0;i<6;i++) {
+		
+		System.out.print("Bucle numero: ");
+		
+		System.out.print(i);
+		
+		System.out.print("\n");
+	}
+
+}
+}
