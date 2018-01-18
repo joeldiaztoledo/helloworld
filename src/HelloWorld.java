e51dd45e6ff768f5d9aeb8d627fa756af0eeac78
Joel Díaz Toledo <34549644+joeldiaztoledo@users.noreply.github.com>
2018-01-18 18:08:47 +0000

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,16 +2,9 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-	
-		int i = 0;
-		for(i=0;i<6;i++) {
 		
-		System.out.print("Bucle numero: ");
+		System.out.print("Hola Mundo.");
 		
-		System.out.print(i);
-		
-		System.out.print("\n");
 	}
 
-}
 }
